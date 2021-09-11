b49eb3729aa8935e0c42ff9eba89723348d7a5e1
odyes <ale.odalism@gmail.com>
2021-09-10 20:15:44 -0700

    Update

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,17 @@
 #include<iostream>
+#include<string>
 
 int main()
 {
+  //gfggit
+   
+  int number;
+  float myFloat;
+  const char* pointerString{"hello"};
+  char charString[]{"world"};
+  std::string stlDtring{"hello World!!!"};
+
+
   std::cout << "Pinters n References: " << std::endl;
   std::cin.get();
   return 0;
